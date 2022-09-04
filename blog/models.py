c67523b18fb79e58949e3f3f92047d18e905aa0b
raziye <raziyehaliakbari@gmail.com>
2022-09-04 15:44:34 +0430

    Changing the language of the site to Farsi

--- blog/models.py
+++ blog/models.py
@@ -4,14 +4,16 @@ from django.db import models
 
 class Article(models.Model):
     STATUS_CHOICES = (
-        ('d','Draft'),
-        ('p','Published'),
+        ('d','پیش نویس'),
+        ('p','منتشر شده'),
     )
-    title=models.CharField(max_length=100)
-    slug=models.SlugField(max_length=50,unique=True)
-    descriptoin=models.TextField()
-    thumnail=models.ImageField(upload_to='images')
-    status=models.CharField(max_length=1,choices=STATUS_CHOICES)
-
+    title=models.CharField(max_length=100 ,verbose_name ="عنوان مقاله")
+    slug=models.SlugField(max_length=50,unique=True, verbose_name="ادرس")
+    descriptoin=models.TextField(verbose_name="توضیحات مقاله")
+    thumnail=models.ImageField(upload_to='images', verbose_name="عکس مقاله")
+    status=models.CharField(max_length=1,choices=STATUS_CHOICES,verbose_name="وضعیت مقاله")
+    class Meta():
+        verbose_name ="مقاله"
+        verbose_name_plural ="مقالات"
     def __str__(self):
         return self.title
